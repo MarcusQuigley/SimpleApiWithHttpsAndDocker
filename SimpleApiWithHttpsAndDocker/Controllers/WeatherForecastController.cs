adb404cdab4174a05da38c606c487239db791b87
Marcus Quigley <marcusquigley@gmail.com>
2021-03-18 19:25:33 -0400

    readding docker file

--- SimpleApiWithHttpsAndDocker/Controllers/WeatherForecastController.cs
+++ SimpleApiWithHttpsAndDocker/Controllers/WeatherForecastController.cs
@@ -24,16 +24,9 @@ namespace SimpleApiWithHttpsAndDocker.Controllers
         }
 
         [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
+        public string Get()
         {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
+            return $"Simple api with docker compose and Https at {DateTime.Now}";
         }
     }
 }
